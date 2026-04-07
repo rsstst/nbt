167f1379d73d13ac835f0d817277fb350e17202a
rsstst <rsstst.ann@gmail.com>
2026-04-07 20:02:20 +0700

    edit: for vercel static

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-// Model
 use App\Models\portfolioModel as pf;
+// Model
 use App\Models\servicesModel as svc;
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
@@ -23,12 +21,15 @@ Route::get('/portfolio', function () {
     return view('portfolio', ['title' => 'Portfolio']);
 });
 
+// 'title', 'imgUrl', 'description', 'slug', 'postType'
 // portfolio detail
 Route::get('/portfolio/it', function () {
-    return view('details.portfolio.it', ['title' => 'Portfolio IT', 'posts' => pf::where('postType', 'IT')->get()]);
+    return view('details.portfolio.it', ['title' => 'Portfolio IT', 'posts' => [['title' => 'a', 'imgUrl' => 'a', 'description' => 'a', 'slug' => 'a', 'postType' => 'a'],
+        ['title' => 'b', 'imgUrl' => 'b', 'description' => 'b', 'slug' => 'b', 'postType' => 'b'], ['title' => 'a', 'imgUrl' => 'a', 'description' => 'a', 'slug' => 'a', 'postType' => 'a'],
+        ['title' => 'c', 'imgUrl' => 'c', 'description' => 'c', 'slug' => 'c', 'postType' => 'c'], ['title' => 'a', 'imgUrl' => 'a', 'description' => 'a', 'slug' => 'a', 'postType' => 'a']]]);
 });
 Route::get('/portfolio/design', function () {
-    return view('details.portfolio.design', ['title' => 'Portfolio Desain', 'posts' => pf::where('postType', 'Design')->get()]);
+    return view('details.portfolio.design', ['title' => 'Portfolio Desain', 'posts' => [['title' => 'a', 'imgUrl' => 'a', 'description' => 'a', 'slug' => 'a', 'postType' => 'a']]]);
 });
 Route::get('/portfolio/it/{post:slug}', function (pf $post) {
     return view('details.it-detail', ['title' => 'Detail Portfolio IT', 'post' => $post]);
@@ -39,10 +40,10 @@ Route::get('/portfolio/design/{post:slug}', function (pf $post) {
 
 // service detail
 Route::get('/services/it', function () {
-    return view('details.services.it', ['title' => 'Layanan IT', 'posts' => svc::where('postType', 'IT')->get()]);
+    return view('details.services.it', ['title' => 'Layanan IT', 'posts' => [['title' => 'a', 'imgUrl' => 'a', 'description' => 'a', 'slug' => 'a', 'postType' => 'a']]]);
 });
 Route::get('/services/design', function () {
-    return view('details.services.design', ['title' => 'Layanan Desain', 'posts' => svc::where('postType', 'Design')->get()]);
+    return view('details.services.design', ['title' => 'Layanan Desain', 'posts' => [['title' => 'a', 'imgUrl' => 'a', 'description' => 'a', 'slug' => 'a', 'postType' => 'a']]]);
 });
 Route::get('/services/it/{post:slug}', function (svc $post) {
     return view('details.it-detail', ['title' => 'Detail Layanan IT', 'post' => $post]);
