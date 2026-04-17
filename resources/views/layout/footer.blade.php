ca58036a69dfe595badf2dba09084cf903856204
rsstst <rsstst.ann@gmail.com>
2026-04-17 20:27:07 +0700

    update: footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,14 +1,34 @@
 <footer>
-        <div class="bg-secondary border border-transparent border-t-button text-white py-8">
-            <div class="container mx-auto px-6 lg:px-10">
-                <div class="flex flex-col md:flex-row items-center justify-between">
-                    <p class="text-sm">&copy; 2024 Naru Branch Tech. All rights reserved.</p>
-                    <div class="flex space-x-4 mt-4 md:mt-0">
-                        <a href="#" class="text-white hover:text-button transition-colors">Layanan Kami</a>
-                        <a href="#" class="text-white hover:text-button transition-colors">Tentang Kami</a>
-                        <a href="#" class="text-white hover:text-button transition-colors">Kontak Kami</a>
-                    </div>
-                </div>
+    <div class="bg-secondary border border-transparent border-t-button text-white py-8">
+        <div class="flex flex-row border border-white w-full p-8">
+            <div class="flex flex-col w-[80%]">
+                <a href="" class="h-full"><img src="https://placehold.co/120x75/red/white?text=Logo+Placeholder"
+                        alt="Narubrancht    ech"></a>
+                <p class="text-sm">&copy; {{ date('Y') }} My Website. All rights reserved.</p>
+            </div>
+            <div class=" w-[25%]">
+                <p class="mb-4">Explore</p>
+                <ul>
+                    <li class="flex flex-col gap-2">
+                        <a href="">Beranda</a>
+                        <a href="">Layanan</a>
+                        <a href="">Portofolio</a>
+                        <a href="">Kontak</a>
+                        <a href="">Tentang Kami</a>
+                    </li>
+                </ul>
+            </div>
+            <div class=" w-[25%]">
+                <p class="mb-4">Connect</p>
+                <ul>
+                    <li class="flex flex-col gap-2">
+                        <a href=""><i class="fa fa-brands fa-whatsapp"></i>Whatsapp</a>
+                        <a href=""><i class="fa fa-brands fa-instagram"></i>Instagram</a>
+                        <a href=""><i class="fa fa-brands fa-tiktok"></i>Tiktok</a>
+                        <a href=""><i class="fa fa-envelope"></i>Email</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </div>
 </footer>
